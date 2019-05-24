36e782be4b0d4a8dad7d67831cab907c85d2b399
Rafael Barbosa <31570331+rafaelxkr@users.noreply.github.com>
2019-05-23 22:09:55 -0300

    Create Selic.m

---
+++ Linguagem M/Mercado Financeiro/Selic.m
@@ -0,0 +1,10 @@
+let
+    Fonte = Json.Document(Web.Contents("https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=json")),
+    #"Convertido para Tabela" = Table.FromList(Fonte, Splitter.SplitByNothing(), null, null, ExtraValues.Error),
+    #"Column1 Expandido" = Table.ExpandRecordColumn(#"Convertido para Tabela", "Column1", {"data", "valor"}, {"data", "valor"}),
+    #"Tipo Alterado com Localidade" = Table.TransformColumnTypes(#"Column1 Expandido", {{"valor", type number}}, "en-US"),
+    #"Tipo Alterado" = Table.TransformColumnTypes(#"Tipo Alterado com Localidade",{{"data", type date}}),
+    #"Linhas Invertidas" = Table.ReverseRows(#"Tipo Alterado"),
+    #"Coluna dividida" = Table.TransformColumns(#"Linhas Invertidas", {{"valor", each _ / 100, type number}})
+in
+    #"Coluna dividida"

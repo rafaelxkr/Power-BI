a2cf61ea194788e76feea393bd12aca28c6bed70
Rafael Barbosa <31570331+rafaelxkr@users.noreply.github.com>
2020-02-08 13:44:55 -0300

    Update Calendario_V1.m

--- Linguagem M/Calendário/Calendario_V1.m
+++ Linguagem M/Calendário/Calendario_V1.m
@@ -1,14 +1,16 @@
-
 let
-    Date.StartOfYear(List.Min(Vendas[Data])),
     DataFinal = List.Max(Vendas[Data]),
-    QtdeDias = Duration.Days(DataFinal - DataInicial) + 1,
-    Lista = List.Dates(DataInicial, QtdeDias, #duration(1,0,0,0)),
-    Tabela = #table(type table[Data=date, Ano=Int32.Type, Nome do Mês=text, Mês=Int32.Type], List.Transform(Lista, each {
+    QtdeDias = Duration.Days(DataFinal - List.Min(Vendas[Data])) + 1,
+    Lista = List.Dates(List.Min(Vendas[Data]), QtdeDias, #duration(1,0,0,0)),
+    Tabela = #table(type table[Data=date, Ano=Int32.Type, Nome do Mês=text, Mês=Int32.Type, Dia = Int8.Type, Semana = Int8.Type, Nome da Semana = text], List.Transform(Lista, each {
         _,
         Date.Year(_),
-        Text.Start(Date.MonthName(_), 3),
-        Date.Month(_)
-        }))
+        Text.Proper(Text.Start(Date.MonthName(_), 3)),
+        Date.Month(_),
+        Date.Day(_),
+        Date.DayOfWeek(_),
+        Text.BeforeDelimiter(Date.DayOfWeekName(_),"-")
+        })),
+    #"Texto Extraído Antes do Delimitador" = Table.TransformColumns(Tabela, {{"Nome da Semana", each Text.BeforeDelimiter(_, "-"), type text}})
 in
-    Tabela
+        #"Texto Extraído Antes do Delimitador"

258c2e8fdf94c9c0ffd378bab08951b228967070
Rafael Barbosa <31570331+rafaelxkr@users.noreply.github.com>
2021-06-11 18:01:25 -0300

    Otimização

--- Linguagem M/Pasta Sharepoint/CSV.m
+++ Linguagem M/Pasta Sharepoint/CSV.m
@@ -1,10 +1,9 @@
+
 let
     Fonte = SharePoint.Files("https://{Dominio}.sharepoint.com/sites/GestoPneu", [ApiVersion = 15]),
     Filtro = Table.SelectRows(Fonte, each Text.Contains([Folder Path], "/OM/KSB1/")), //filtrar a pasta
-    Remover = Table.SelectColumns(Filtro,{"Content"}), // Manter somente a coluna "Content" 
-    Excel = Table.TransformColumns(Remover,{"Content", each Csv.Document(_,[Delimiter=";", Encoding=1252, QuoteStyle=QuoteStyle.None])}),//Abre arquivo CSV
-    Cabecalho = Table.TransformColumns(Excel,{"Content", Table.PromoteHeaders}),//1° Linha como Cabeçalho
-    Expandir = Table.ExpandTableColumn(Cabecalho,"Content", Table.ColumnNames(Cabecalho{0}[Content])),//expandir as colunas
-    Tipo = Table.TransformColumnTypes(Expandir, List.Zip({Table.ColumnNames(Expandir),List.Repeat({type text},Table.ColumnCount(Expandir))}))//alterar tipo para texto 
+    CSV = Table.TransformColumns(Filtro[[Content]],{"Content", each Csv.Document(_,[Delimiter=";", Encoding=1252, QuoteStyle=QuoteStyle.None])}), //Abre arquivo CSV
+    Expandir = Table.ExpandTableColumn(Excel,"Content", Table.ColumnNames(Cabecalho{0}[Content])),//expandir as colunas
+    Tipo =  Table.TransformColumnTypes(Expandir, List.Zip({Table.ColumnNames(Expandir),List.Repeat({type text},Table.ColumnCount(Expandir))}))//alterar tipo para texto 
 in
     Tipo

6ddf8a8911843d3fe1ae1ec64423332ea0acbf8d
Rafael Barbosa <31570331+rafaelxkr@users.noreply.github.com>
2021-06-11 23:06:28 -0300

    Update CSV.m

--- Linguagem M/Pasta Sharepoint/CSV.m
+++ Linguagem M/Pasta Sharepoint/CSV.m
@@ -1,10 +1,10 @@
 
 let
-    Fonte = SharePoint.Files("https://{Dominio}.sharepoint.com/sites/GestoPneu", [ApiVersion = 15]),
-    Filtro = Table.SelectRows(Fonte, each Text.Contains([Folder Path], "/OM/KSB1/")), //filtrar a pasta
-    CSV = Table.TransformColumns(Filtro[[Content]],{"Content", each Csv.Document(_,[Delimiter=";", Encoding=1252, QuoteStyle=QuoteStyle.None])}), //Abre arquivo CSV
-    Cabecalho = Table.TransformColumns(CSV,{"Content", each Csv.Document(_,[Delimiter=";", Encoding=1252, QuoteStyle=QuoteStyle.None])}),
-    Expandir = Table.ExpandTableColumn(Cabecalho,{"Content", Table.PromoteHeaders}),//expandir as colunas
+    Fonte = SharePoint.Files("https://votorantimindustrial.sharepoint.com/sites/GestoPneu", [ApiVersion = 15]),
+    Filtro = Table.SelectRows(Fonte, each Text.Contains([Folder Path], "/Exemplos/Abrir arquivo ZIP/")), //filtrar a pasta
+    CSV = Table.TransformColumns(Filtro[[Content]],{"Content", each Csv.Document(_,[Delimiter=",", Encoding=1252, QuoteStyle=QuoteStyle.None])}), //Abre arquivo CSV
+    Cabecalho = Table.TransformColumns(CSV,{"Content", Table.PromoteHeaders}),
+    Expandir = Table.ExpandTableColumn(Cabecalho,"Content", Table.ColumnNames(Cabecalho{0}[Content])),//expandir as colunas
     Tipo =  Table.TransformColumnTypes(Expandir, List.Zip({Table.ColumnNames(Expandir),List.Repeat({type text},Table.ColumnCount(Expandir))}))//alterar tipo para texto 
 in
     Tipo

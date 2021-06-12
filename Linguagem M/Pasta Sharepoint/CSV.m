2bc1c470e1365df097c2f8ed84e21f9a32cbe754
Rafael Barbosa <31570331+rafaelxkr@users.noreply.github.com>
2021-06-11 22:55:02 -0300

    Update CSV.m

--- Linguagem M/Pasta Sharepoint/CSV.m
+++ Linguagem M/Pasta Sharepoint/CSV.m
@@ -3,7 +3,8 @@ let
     Fonte = SharePoint.Files("https://{Dominio}.sharepoint.com/sites/GestoPneu", [ApiVersion = 15]),
     Filtro = Table.SelectRows(Fonte, each Text.Contains([Folder Path], "/OM/KSB1/")), //filtrar a pasta
     CSV = Table.TransformColumns(Filtro[[Content]],{"Content", each Csv.Document(_,[Delimiter=";", Encoding=1252, QuoteStyle=QuoteStyle.None])}), //Abre arquivo CSV
-    Expandir = Table.ExpandTableColumn(Excel,"Content", Table.ColumnNames(Cabecalho{0}[Content])),//expandir as colunas
+    Cabecalho = Table.TransformColumns(CSV,{"Content", each Csv.Document(_,[Delimiter=";", Encoding=1252, QuoteStyle=QuoteStyle.None])}),
+    Expandir = Table.ExpandTableColumn(Cabecalho,{"Content", Table.PromoteHeaders}),//expandir as colunas
     Tipo =  Table.TransformColumnTypes(Expandir, List.Zip({Table.ColumnNames(Expandir),List.Repeat({type text},Table.ColumnCount(Expandir))}))//alterar tipo para texto 
 in
     Tipo
